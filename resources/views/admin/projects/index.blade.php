9ba23f121872683e32de530febfd761c429d26f3
WebGCDev <gaetano.chi@gmail.com>
2024-02-06 19:54:14 +0100

    Add: Graphic Start

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -3,7 +3,13 @@
 @section('content')
  <ul>
     @foreach ($projects as $project)
-        <li>{{$project->title}}</li>
+        <li>TITOLO: {{$project->title}}</li>
+        <li>AUTORE: {{$project->author}}</li>
+        <li>DATA CREAZIONE: {{$project->creation_date}}</li>
+        <li>DATA ULTIMO UPDATE: {{$project->last_update}}</li>
+        <li>LINGUAGGI DI PROGRAMMAZIONE : {{$project->lang}}</li>
+        <li>LINK GITHUB: {{$project->link_github}}</li>
+        <br>
     @endforeach
   </ul>
 @endsection
